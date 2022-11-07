ba6694c70743f8f287224a5bfd0d9525efebd437
Chip <clempke90@gmail.com>
2022-11-07 16:14:15 -0500

    added months and days

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 let now = new Date();
 let TimeStart = Date.now();
+months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 // javascript.info/date
+// Date.parse(str);
