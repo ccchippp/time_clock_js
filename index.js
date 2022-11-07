65dda616fbc3ca7ba603da74c914251c26141848
Chip <clempke90@gmail.com>
2022-11-07 16:59:33 -0500

    year calculation complete - month calc needs testing

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+timestamp = {
+  timecode: now,
+  month: "",
+  day: "",
+  year: "",
+};
+
 let now = new Date();
 let TimeStart = Date.now();
 months = [
@@ -23,6 +30,18 @@ days = [
   "Friday",
   "Saturday",
 ];
+Math.trunc((now / 1000 / 3600 / 24) % 12);
+let currentYear = Math.trunc(now / yearInMiliseconds) + 1970;
 
+let yearInMiliseconds = 31556926000;
+let monthInMiliseconds = 2629743833.3334;
+
+let themonth = now / 2629743833.3334 / 12; //now divided by number of miliseconds in month divided by month
+
+function crush(themonth) {
+  themooonth = themonth % 1; //grab months leftover from year
+  themooonth = themoonth * 12; //multiply by 12 to get month...probably round down
+  return months[Math.floor(themooonth)];
+}
 // javascript.info/date
 // Date.parse(str);
