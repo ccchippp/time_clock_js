227cccb7d550792c389157e9840932735fc5d025
Chip <clempke90@gmail.com>
2022-11-08 11:46:45 -0500

    new month object, needs testing

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ days = [
   "Friday",
   "Saturday",
 ];
+let month = {
+  number: now.getMonth(),
+  name: months[now.getMonth()],
+};
+
 Math.trunc((now / 1000 / 3600 / 24) % 12);
 let currentYear = Math.trunc(now / yearInMiliseconds) + 1970;
 
@@ -38,10 +43,21 @@ let monthInMiliseconds = 2629743833.3334;
 
 let themonth = now / 2629743833.3334 / 12; //now divided by number of miliseconds in month divided by month
 
+(month) => {
+  month = month % 1;
+  console.log(month);
+
+  month = month * 12;
+  console.log(month);
+
+  months[Math.floor(month)];
+  console.log(month);
+};
+
 function crush(themonth) {
   themooonth = themonth % 1; //grab months leftover from year
   themooonth = themoonth * 12; //multiply by 12 to get month...probably round down
-  return months[Math.floor(themooonth)];
+  return months[Math.floor(themooonth)]; //round down int from previous calcs and apply to month array
 }
 // javascript.info/date
 // Date.parse(str);
